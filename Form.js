3a91c657f94bd7461de9a84de142583f6b17706e
Jegorka2021 <86378869+Jegorka2021@users.noreply.github.com>
2025-05-29 21:35:25 +0200

    Update Form.js

--- Form.js
+++ Form.js
@@ -8,3 +8,42 @@ document.getElementById('tripForm').addEventListener('submit', function(e) {
     alert('Please choose a trip!');
   }
 });
+
+// Проверка сохранённой темы при загрузке
+if (localStorage.getItem('theme') === 'dark') {
+  document.body.classList.add('dark-theme');
+  document.getElementById('themeToggle').checked = true;
+}
+
+document.getElementById('themeToggle').addEventListener('change', function () {
+  if (this.checked) {
+    document.body.classList.add('dark-theme');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.body.classList.remove('dark-theme');
+    localStorage.setItem('theme', 'light');
+  }
+});
+const nav = document.getElementById('mainNav');
+
+toggle.addEventListener('change', () => {
+  if (toggle.checked) {
+    document.body.classList.add('dark-theme');
+    localStorage.setItem('theme', 'dark');
+
+    // меняем тему навбара
+    nav.classList.remove('navbar-light', 'bg-light');
+    nav.classList.add('navbar-dark', 'bg-dark');
+  } else {
+    document.body.classList.remove('dark-theme');
+    localStorage.setItem('theme', 'light');
+
+    // возвращаем светлую тему навбара
+    nav.classList.remove('navbar-dark', 'bg-dark');
+    nav.classList.add('navbar-light', 'bg-light');
+  }
+});
+// // Theme toggle
+// document.getElementById('themeToggle').addEventListener('change', function () {
+//     document.body.classList.toggle('dark-theme', this.checked);
+// });
